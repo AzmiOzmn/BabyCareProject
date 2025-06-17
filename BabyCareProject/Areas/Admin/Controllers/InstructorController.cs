a92e9b9505a6ac4f3a9541a88d21540e95a1e780
Azmi <azmiozmen5234@gmail.com>
2025-06-17 23:26:57 +0300

    update

---
+++ BabyCareProject/Areas/Admin/Controllers/InstructorController.cs
@@ -0,0 +1,14 @@
+﻿using BabyCareProject.Services.InstructorServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BabyCareProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class InstructorController(IInstructorService _instructorService) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
