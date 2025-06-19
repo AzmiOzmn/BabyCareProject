10d57c9ee74c1e7f4cc9acb66bc2b7c7b9bad39a
Azmi <azmiozmen5234@gmail.com>
2025-06-19 20:29:29 +0300

    update

---
+++ BabyCareProject/Mappings/ProductMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using BabyCareProject.DataAccess.Entities;
+using BabyCareProject.Dtos.ProductDtos;
+
+namespace BabyCareProject.Mappings
+{
+    public class ProductMapping : Profile
+
+    {
+        public ProductMapping()
+        {
+            CreateMap<Product,CreateProductDto>().ReverseMap(); 
+            CreateMap<Product,UpdateProductDto>().ReverseMap(); 
+            CreateMap<Product,ResultProductDto>().ReverseMap(); 
+
+        }
+    }
+}
---
+++ BabyCareProject/Services/ProductSerrvices/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using BabyCareProject.Dtos.ProductDtos;
+
+namespace BabyCareProject.Services.ProductSerrvices
+{
+    public interface IProductService
+    {
+        Task<List<ResultProductDto>> GetAllAsync();
+
+        Task<UpdateProductDto> GetById(string id);
+
+        Task CreateAsycn(CreateProductDto createProductDto);
+
+        Task UpdateAsync(UpdateProductDto updateProductDto);
+
+        Task DeleteAsync(string id);
+
+
+    }
+}
